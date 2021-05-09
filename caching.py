67f20c583c52fb330ab622108f21f6dc7d981aa7
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-05-09 21:11:49 +0530

    ♻: Blackned

--- caching.py
+++ caching.py
@@ -3,13 +3,15 @@ import time
 import requests
 import json
 
+
 def offline():
     try:
-        requests.get("https://duckduckgo.com")        
+        requests.get("https://duckduckgo.com")
     except requests.ConnectionError:
         print("offline")
         return True
     return False
+
 
 def get_rate():
     if read_cache()[0]:
@@ -19,29 +21,31 @@ def get_rate():
         get_rate()
     return response
 
-def make_cache(base='', cache_name='cache.json'):
+
+def make_cache(base="", cache_name="cache.json"):
     # now = date.today()
     # now.strftime("%m-%d-%y")
-    
-    response = requests.get(f'https://api.exchangeratesapi.io/latest?base={base}')
-    
-    if response.status_code != 200:
-        raise ApiError(f'Some Error')
-     
-    response = response.json() 
 
-    response['time'] = time.time()
-    with open(cache_name, 'w') as f:
+    response = requests.get(f"https://api.exchangeratesapi.io/latest?base={base}")
+
+    if response.status_code != 200:
+        raise ApiError(f"Some Error")
+
+    response = response.json()
+
+    response["time"] = time.time()
+    with open(cache_name, "w") as f:
         json.dump(response, f)
 
-def read_cache(cache_name='cache.json'):
+
+def read_cache(cache_name="cache.json"):
     now = time.time()
     if path.exists(f"./{cache_name}"):
-        with open(cache_name,'r') as f:
+        with open(cache_name, "r") as f:
             cached_response = json.load(f)
-            cached_time = cached_response['time']
+            cached_time = cached_response["time"]
             if offline():
-                return [True, cached_response]        
+                return [True, cached_response]
             elif now - cached_time > 3600:
                 return [False]
             else:
--- curconv.py
+++ curconv.py
@@ -1,19 +1,22 @@
 from caching import *
-import json 
+import json
 import sys
+
 # api url = 'https://api.exchangeratesapi.io/latest'
-    
+
+
 def main():
-    amt = float(sys.argv [1])
+    amt = float(sys.argv[1])
     base_cur = sys.argv[2]
     target_cur = sys.argv[3]
 
-    rates = get_rate()['rates']
+    rates = get_rate()["rates"]
 
     base_rate = rates[base_cur.upper()]
     target_rate = rates[target_cur.upper()]
     final = amt / base_rate * target_rate
-    print(round(final, 4 ), target_cur)
+    print(round(final, 4), target_cur)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
